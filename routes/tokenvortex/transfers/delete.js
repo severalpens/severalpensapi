5c4d9058feaeee5aa26c886e3d32d4ebda84e631
Paul <paulcollinsaf@gmail.com>
2020-04-17 11:29:23 +1000

    develop branch bugfix

--- routes/tokenvortex/transfers/delete.js
+++ routes/tokenvortex/transfers/delete.js
@@ -2,17 +2,41 @@ var express = require("express");
 var router = express.Router();
 var bodyParser = require("body-parser");
 var TransfersModel = require('../models/mongodb/transfers');
+var TransactionsModel = require('../models/mongodb/transactions');
 
 var cors = require('cors');
 router.use(cors());
 
 router.post("/:_id", bodyParser.json(), function(req, res, next) {
     let _id = req.params._id;
-    TransfersModel.updateOne({ _id }, { isActive: false }).then(result => {
-      return res.send(result);
+    console.log(_id);
+    TransfersModel.deleteOne({_id},(err) => {
+      if(err){
+        console.log(err);
+        res.send(err);
+      }
+      console.log(`about to send ${_id}`);
+      res.send(_id)
     });
   });
    
+
+  router.delete("/:_id", bodyParser.json(), function(req, res, next) {
+    let _id = req.params._id;
+    TransfersModel.deleteOne({_id},(err) => {
+      if(err){
+        console.log(err);
+        return res.send(err);
+      }
+      TransactionsModel.delete({transfer_id: _id},(err) => {
+        if(err){
+          console.log(err);
+          return res.send(err);
+        } 
+      })
+    });
+  });
+
 
 
 
