4ce6b31ad5772e4901918e630d72b2039b70682c
severalpens <paulcollinsaf@gmail.com>
2020-05-14 20:38:30 +1000

    develop

--- routes/tokenvortex/tokenvortex.js
+++ routes/tokenvortex/tokenvortex.js
@@ -6,6 +6,7 @@ var accountsRouter = require('./accounts/accounts');
 var contractsRouter = require('./contracts/contracts');
 var transfersRouter = require('./transfers/transfers');
 var transactionsRouter = require('./transactions/transactions');
+const { generateKeyPair } = require('crypto');
 
 var cors = require('cors');
 router.use(cors());
@@ -15,5 +16,31 @@ router.use('/users', usersRouter);
 router.use('/contracts', contractsRouter);
 router.use('/transfers', transfersRouter);
 router.use('/transactions', transactionsRouter);
+
+
+router.get('/generatekeypair/:_id', function(req, res) {
+    let tmpPassphrase = req.params._id || '';
+    generateKeyPair('rsa', {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: tmpPassphrase
+      }
+    }, (err, public, private) => {
+        let privateKey = private.split('-----')[2];
+        let publicKey = public.split('-----')[2];
+        let result = {privateKey,publicKey};
+        console.log(result);
+        res.send(result);
+    });
+  });
+    
+  
 
 module.exports = router;
