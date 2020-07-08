b8b8a0349d13a26316fae7ead4903aeb090f76e9
severalpens <paulcollinsaf@gmail.com>
2020-07-08 20:09:39 +1000

    develop

--- routes/tokenvortex/transactions/balance.js
+++ routes/tokenvortex/transactions/balance.js
@@ -3,9 +3,6 @@ const ContractsModel = require("../models/mongodb/contracts");
 const AccountsModel = require("../models/mongodb/accounts");
 
 async function getBalance(network,publisherAccount,contractAddress, contract, publisherAddress   ){
-  let provider = ethers.getDefaultProvider(network);
-  let wallet = new ethers.Wallet(publisherAccount.privateKey, provider);
-  let ethersContract = new ethers.Contract(contractAddress, JSON.parse(contract.abi), wallet);
   let balance = await ethersContract.balanceOf(publisherAddress);
   console.log(balance);
 }
