1f1cb293e21a73150351549f7c336496f74949eb
severalpens <paulcollinsaf@gmail.com>
2020-07-15 15:43:25 +1000

    add initial migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const TokenVorex = artifacts.require("TokenVortex");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(TokenVorex);
 };
