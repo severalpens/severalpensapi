978134b89696940812c85f1fa612c3f907089b66
severalpens <paulcollinsaf@gmail.com>
2020-07-24 19:45:26 +1000

    develop

--- truffle-config.js
+++ truffle-config.js
@@ -43,9 +43,9 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
+     host: "localhost",     // 127.0.0.1 (default: none)
      port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     network_id: "4447",       // Any network (default: none)
     },
 
     // Another network with more advanced options...
