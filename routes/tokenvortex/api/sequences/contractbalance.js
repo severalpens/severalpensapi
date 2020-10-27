ff6e7513e18629ff807b5d50d304a9d3554920e5
severalpens <paulcollinsaf@gmail.com>
2020-10-27 13:34:43 +1000

    entities2

--- routes/tokenvortex/api/sequences/contractbalance.js
+++ routes/tokenvortex/api/sequences/contractbalance.js
@@ -15,35 +15,21 @@ router.use(bodyParser.json({ extended: false }));
 
 router.post("/", async function (req, res) {
   let x = req.body.balanceQuery;
-  let contract = await getContract(req.body.contract_id, req.user_id);
+  let contract = await EntitiesModel.findById(req.body.contract_id).lean().exec();
   let addressSource = {}
   let address = '';
-  if (x.type === 'contract') {
-    addressSource = await getContract(req.body.balanceQuery._id, req.user_id);
-    address = addressSource.addresses[req.body.network];
-  }
-
-  if (x.type === 'account') {
-    addressSource = await  getAccount(req.body.balanceQuery._id, req.user_id);
-    address = addressSource.address;
-  }
-
+  addressSource = await EntitiesModel.findById(req.body.balanceQuery._id).lean().exec();
+  address = x.type === 'contract' ? addressSource.body.addresses[req.body.network] : addressSource.body.address;
   let provider = new ethers.providers.InfuraProvider(req.body.network, 'abf62c2c62304ddeb3ccb2d6fb7a8b96');
   let wallet = new ethers.Wallet('0xa2e19e3c8580f31b13ad31fa1638a503bfc6771be57eb1952b3f9416ad07194a', provider);
-  let ethersContract = new ethers.Contract(contract.addresses[req.body.network], contract.abi, wallet);
+  let ethersContract = new ethers.Contract(contract.body.addresses[req.body.network], contract.body.abi, wallet);
   let balance = await ethersContract.balanceOf(address);
   res.send(balance);
-
-
 });
 
 async function getContract(_id, user_id) {
-  const contractsQuery = EntitiesModel.findById(_id);
-  contractsQuery.setOptions({ lean: true });
-  contractsQuery.collection(EntitiesModel.collection);
-  contractsQuery.or([{ user_id: 'public' }, { user_id }]);
-  contractsQuery.where('isActive').equals(true);
-  return await contractsQuery.exec();
+ let result =  EntitiesModel.findOne({_id,user_id}).lean().exec();
+ return result;
 }
 
 
--- routes/tokenvortex/tests/entities-test.js
+++ routes/tokenvortex/tests/entities-test.js
@@ -1,26 +1,25 @@
 process.env.NODE_ENV = 'test';
 
 let mongoose = require("mongoose");
-let Book = require('../app/models/book');
+let EntitiesModel = require('../models/entities');
 
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../server');
-let should = chai.should();
-
+// let server = require('../server');
+let server  = require('../../../bin/www')
 
 chai.use(chaiHttp);
 
-describe('Books', () => {
+describe('Entities', () => {
     beforeEach((done) => {
-        Book.remove({}, (err) => {
+        EntitiesModel.remove({}, (err) => {
            done();
         });
     });
-  describe('/GET book', () => {
-      it('it should GET all the books', (done) => {
+  describe('/GET entities', () => {
+      it('it should GET all the entities', (done) => {
             chai.request(server)
-            .get('/book')
+            .get('/entities')
             .end((err, res) => {
                   res.should.have.status(200);
                   res.body.should.be.a('array');
@@ -29,16 +28,16 @@ describe('Books', () => {
             });
       });
   });
-  describe('/POST book', () => {
-      it('it should not POST a book without pages field', (done) => {
-          let book = {
-              title: "The Lord of the Rings",
-              author: "J.R.R. Tolkien",
-              year: 1954
+  describe('/POST entity', () => {
+      it('it should not POST a entity without pages field', (done) => {
+          let entity = {
+              name: "The Lord of the Rings",
+              body: {},
+              user_id: 'user_id'
           }
             chai.request(server)
-            .post('/book')
-            .send(book)
+            .post('/entities')
+            .send(entity)
             .end((err, res) => {
                   res.should.have.status(200);
                   res.body.should.be.a('object');
@@ -48,35 +47,35 @@ describe('Books', () => {
               done();
             });
       });
-      it('it should POST a book ', (done) => {
-          let book = {
+      it('it should POST a entity ', (done) => {
+          let entity = {
               title: "The Lord of the Rings",
               author: "J.R.R. Tolkien",
               year: 1954,
               pages: 1170
           }
             chai.request(server)
-            .post('/book')
-            .send(book)
+            .post('/entities')
+            .send(entity)
             .end((err, res) => {
                   res.should.have.status(200);
                   res.body.should.be.a('object');
-                  res.body.should.have.property('message').eql('Book successfully added!');
-                  res.body.book.should.have.property('title');
-                  res.body.book.should.have.property('author');
-                  res.body.book.should.have.property('pages');
-                  res.body.book.should.have.property('year');
+                  res.body.should.have.property('message').eql('EntitiesModel successfully added!');
+                  res.body.entity.should.have.property('title');
+                  res.body.entity.should.have.property('author');
+                  res.body.entity.should.have.property('pages');
+                  res.body.entity.should.have.property('year');
               done();
             });
       });
   });
-  describe('/GET/:id book', () => {
-      it('it should GET a book by the given id', (done) => {
-          let book = new Book({ title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954, pages: 1170 });
-          book.save((err, book) => {
+  describe('/GET/:id entity', () => {
+      it('it should GET a entity by the given id', (done) => {
+          let entity = new EntitiesModel({ title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954, pages: 1170 });
+          entity.save((err, entity) => {
               chai.request(server)
-            .get('/book/' + book.id)
-            .send(book)
+            .get('/entity/' + entity.id)
+            .send(entity)
             .end((err, res) => {
                   res.should.have.status(200);
                   res.body.should.be.a('object');
@@ -84,25 +83,25 @@ describe('Books', () => {
                   res.body.should.have.property('author');
                   res.body.should.have.property('pages');
                   res.body.should.have.property('year');
-                  res.body.should.have.property('_id').eql(book.id);
+                  res.body.should.have.property('_id').eql(entity.id);
               done();
             });
           });
 
       });
   });
-  describe('/PUT/:id book', () => {
-      it('it should UPDATE a book given the id', (done) => {
-          let book = new Book({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1948, pages: 778})
-          book.save((err, book) => {
+  describe('/PUT/:id entity', () => {
+      it('it should UPDATE a entity given the id', (done) => {
+          let entity = new EntitiesModel({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1948, pages: 778})
+          entity.save((err, entity) => {
                 chai.request(server)
-                .put('/book/' + book.id)
+                .put('/entity/' + entity.id)
                 .send({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1950, pages: 778})
                 .end((err, res) => {
                       res.should.have.status(200);
                       res.body.should.be.a('object');
-                      res.body.should.have.property('message').eql('Book updated!');
-                      res.body.book.should.have.property('year').eql(1950);
+                      res.body.should.have.property('message').eql('EntitiesModel updated!');
+                      res.body.entity.should.have.property('year').eql(1950);
                   done();
                 });
           });
@@ -111,16 +110,16 @@ describe('Books', () => {
  /*
   * Test the /DELETE/:id route
   */
-  describe('/DELETE/:id book', () => {
-      it('it should DELETE a book given the id', (done) => {
-          let book = new Book({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1948, pages: 778})
-          book.save((err, book) => {
+  describe('/DELETE/:id entity', () => {
+      it('it should DELETE a entity given the id', (done) => {
+          let entity = new EntitiesModel({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1948, pages: 778})
+          entity.save((err, entity) => {
                 chai.request(server)
-                .delete('/book/' + book.id)
+                .delete('/entity/' + entity.id)
                 .end((err, res) => {
                       res.should.have.status(200);
                       res.body.should.be.a('object');
-                      res.body.should.have.property('message').eql('Book successfully deleted!');
+                      res.body.should.have.property('message').eql('EntitiesModel successfully deleted!');
                       res.body.result.should.have.property('ok').eql(1);
                       res.body.result.should.have.property('n').eql(1);
                   done();
